2d22a930e2325f476674d4a62c807a958b0b1c03
Marco Huerta <marco.huerta.sc@pm.me>
2021-07-16 22:00:57 -0500

    MetodosVoid

---
+++ Metodos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Metodos
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            EscribeAlgo();
+            EscribeAlgoEspecifico("Soy un argumento y me llamaron desde un método");
+            Console.Read();
+            
+        }
+
+        //Estructura de un Método-- NivelDeAcceso - (static) - TipoDeRetorno - NombreDelMetodo (Parámetro1, Parámetro2)
+        public static void EscribeAlgo()
+        {
+            Console.WriteLine("Yo nací en un método");
+            
+        }
+
+        public static void EscribeAlgoEspecifico(string miTexto)
+        {
+            Console.WriteLine(miTexto);
+        }
+    }
+}
